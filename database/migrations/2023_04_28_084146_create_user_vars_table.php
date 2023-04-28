e64a7f3f6dfa3a6c53ee26bde05a8bf60cb52ad4
Rachel <rachet2002@hotmail.com>
2023-04-28 15:59:29 +0700

    First

---
+++ database/migrations/2023_04_28_084146_create_user_vars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_vars', function (Blueprint $table) {
+            $table->id();
+            $table->string('var_name', 100);
+            $table->string('var_value');
+            $table->integer('officer_id');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_vars');
+    }
+};
