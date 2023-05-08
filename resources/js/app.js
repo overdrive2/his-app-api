ed191519103430795bd2141a46f7c75ae209bab8
Rachel <rachet2002@hotmail.com>
2023-05-08 16:10:04 +0700

    make layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import 'flowbite';
-
+import './dark-mode';
+import './sidebar';
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,18 +12,27 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <script>
+
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        </script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
         @laravelPWA
     </head>
-    <body class="font-sans antialiased">
-        <x-banner />
-
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <x-nav-menu :user="Auth::user()" />
-
+    <body class="bg-gray-50 dark:bg-gray-800">
+        <x-nav-menu :user="Auth::user()" />
+        <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+            <x-aside />
+            <div class="fixed inset-0 z-10 bg-gray-900/50 dark:bg-gray-900/90 hidden" id="sidebarBackdrop"></div>
+        </div>
+        <div id="main-centent" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
@@ -32,13 +41,16 @@
                     </div>
                 </header>
             @endif
-
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
-        </div>
 
+            <!-- Page Footer -->
+            @if(isset($footer))
+                {{ $footer }}
+            @endif
+        </div>
         @stack('modals')
 
         @livewireScripts
