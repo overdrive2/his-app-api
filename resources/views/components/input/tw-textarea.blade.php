bbbac6958c0e30e62f66e8d729ce22fa1fd65a84
Rachel <rachet2002@hotmail.com>
2023-07-06 16:31:15 +0700

    textarea component

---
+++ resources/views/components/input/tw-textarea.blade.php
@@ -0,0 +1,19 @@
+@props(['id', 'label'])
+<div
+    x-data="{
+        model: @entangle($attributes->wire('model')),
+    }"
+    wire:ignore
+    class="relative mb-3" data-te-input-wrapper-init>
+    <textarea
+        x-bind:value="model"
+        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+        id="{{ $id ?? 'texta1' }}"
+        rows="3"
+        placeholder=""></textarea>
+    <label
+        for="{{ $id ?? '' }}"
+        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+      >{{  $label ?? ' ' }}</label
+    >
+</div>
