14c474264c8720c3751fb8182030ce95a10db8cd
Rachel <rachet2002@hotmail.com>
2023-08-03 17:27:00 +0700

    asm 80%

--- resources/views/livewire/admin/asm-detail.blade.php
+++ resources/views/livewire/admin/asm-detail.blade.php
@@ -114,26 +114,49 @@
                 <template x-if="curRow.ref === true">
                     <div class="mt-2 flex flex-col">
                         <template x-for="refOpt in curRow.refOpts">
-                            <template x-if="refOpt.type == 'text'">
-                                <div
-                                    x-data
-                                    x-init="
-                                        new Input($refs.textInput)
-                                    "
-                                >
-                                <div x-ref="textInput" class="relative mb-3 xl:w-96" data-te-input-wrapper-init>
-                                    <x-input.text
-                                        x-bind:id="'text-input'+curRow.key+refOpt.key"
-                                        type="text"
-                                    />
-                                    <x-input.label
-                                      x-bind:for="'text-input'+curRow.key+refOpt.key"
-                                      x-text="refOpt.label"
+                            <div class="w-full">
+                                <template x-if="refOpt.type == 'text'">
+                                    <div
+                                        x-data
+                                        x-init = "new Input($refs.textInput)"
                                     >
-                                    </x-input.label>
-                                  </div>
-                                </div>
-                            </template>
+                                        <div x-id="['text-input']" x-ref="textInput" class="relative mb-3" data-te-input-wrapper-init>
+                                            <input
+                                                type="text"
+                                                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                                :id="$id('text-input')"
+                                                x-bind:placeholder="refOpt.label + '...'"
+                                            />
+                                            <label
+                                                :for="$id('text-input')"
+                                                x-text="refOpt.label"
+                                                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                                            >
+                                            </label>
+                                        </div>
+                                    </div>
+                                </template>
+                                <template x-if="refOpt.type == 'radio'">
+                                    <div class="flex justify-start">
+                                        <template x-for="rdItem in refOpt.jsonData">
+                                            <div x-id="['radio-input']" class="mb-[0.125rem] mr-4 inline-block min-h-[1.5rem] pl-[1.5rem]">
+                                                <input
+                                                    class="relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:border-primary dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                                                    type="radio"
+                                                    :id="$id('radio-input')"
+                                                    name="radio-input-{{ $key }}"
+                                                    x-bind:value="rdItem.key"
+                                                />
+                                                <label
+                                                    :for="$id('radio-input')"
+                                                    class="mt-px inline-block pl-[0.15rem] hover:cursor-pointer"
+                                                    x-text="rdItem.value"
+                                                    ></label>
+                                            </div>
+                                        </template>
+                                    </div>
+                                </template>
+                            </div>
                         </template>
                     </div>
                 </template>
